dc3317e589c7cb7a60bdbfe5ffdffb01777b6ca4
mohammad <mohammad.zubair.omari@tekschool.us>
2023-08-19 17:24:12 -0700

    new change

--- src/git_project1/New_GIT_Project_Class.java
+++ src/git_project1/New_GIT_Project_Class.java
@@ -3,7 +3,7 @@ package git_project1;
 public class New_GIT_Project_Class {
 
 	public static void main(String[] args) {
-		System.out.println("This a new java project");
+		System.out.println("This is a new java project");
 
 	}
 
