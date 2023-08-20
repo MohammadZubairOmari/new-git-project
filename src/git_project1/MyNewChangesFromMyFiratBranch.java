4d7c1722443feb083a77172a929bb2131b19015e
mohammad <mohammad.zubair.omari@tekschool.us>
2023-08-19 18:15:14 -0700

    new change from mynewbarnch

---
+++ src/git_project1/MyNewChangesFromMyFiratBranch.java
@@ -0,0 +1,10 @@
+package git_project1;
+
+public class MyNewChangesFromMyFiratBranch {
+
+	public static void main(String[] args) {
+		System.out.println("The new change from my first branch");
+
+	}
+
+}
